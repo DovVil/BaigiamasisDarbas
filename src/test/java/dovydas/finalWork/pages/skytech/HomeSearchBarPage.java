7050a473c4c457169522a19c67218b035590d6af
Dovyduko kompiuteris <dovydas0417@gmail.com>
2022-10-14 12:29:25 +0300

    Add open,sleep methods in HomeSearchBarPage class

---
+++ src/test/java/dovydas/finalWork/pages/skytech/HomeSearchBarPage.java
@@ -0,0 +1,15 @@
+package dovydas.finalWork.pages.skytech;
+
+import dovydas.finalWork.pages.Common;
+
+public class HomeSearchBarPage {
+
+    public static void open(String url) {
+        Common.openUrl(url);
+    }
+
+    public static void sleep(int millis) {
+        Common.sleep(millis);
+    }
+
+}
